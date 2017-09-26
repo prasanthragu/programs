049620107b21907c59ca7ff4b866b6dc900a538f
prasanthragu <32304325+prasanthragu@users.noreply.github.com>
2017-09-26 09:07:46 -0700

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char str;
+scanf("%c",&str);
+if(str=='a'||str=='e'||str=='i'||str=='o'||str=='u')
+{
+printf("char is vowel");
+}
+else
+{
+printf("char is constant");
+}
+return 0;
+}
